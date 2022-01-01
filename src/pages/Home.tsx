00fd7cb4683cea0bd60c1dad05ce1a4bf6338833
Stanley <smuzh001@ucr.edu>
2022-01-01 15:52:23 -0800

    new format init tailwind

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Home() {
+  //Header
+  //hero
+  // work section
+  // projects
+  // about me
+  //contact
+  return <h1 className='text-3xl font-bold'>Hello World</h1>;
+}
